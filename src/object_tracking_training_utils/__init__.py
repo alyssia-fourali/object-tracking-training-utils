3ff12dd54eb5283d9218518ec7a4ed2fdf15b70f
alyssiafourali <alyssia.fourali.etu@univ-lille.fr>
2025-06-01 10:56:55 +0200

    chore: expose core submodules in package __init__.py for easier access

--- src/object_tracking_training_utils/__init__.py
+++ src/object_tracking_training_utils/__init__.py
@@ -3,3 +3,8 @@
 __author__ = """Alyssia Fourali"""
 __email__ = 'alyssia.fourali.etu@univ-lille.fr'
 __version__ = '0.1.0'
+
+from .io import *
+from .transforms import *
+from .visualization import *
+from .utils import *
